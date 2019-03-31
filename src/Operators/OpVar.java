a730c785586d3d37758e9ebe723a4364fe00007f
Zap1999 <45321175+Zap1999@users.noreply.github.com>
2019-03-31 23:37:50 +0300

    not working commit

--- src/Operators/OpVar.java
+++ src/Operators/OpVar.java
@@ -3,7 +3,6 @@ package Operators;
 import Operators.Operator;
 
 import javax.script.ScriptException;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class OpVar extends Operator {
@@ -16,23 +15,12 @@ public class OpVar extends Operator {
     public void exec(Interpretater inte) {
         String[] parts = code.split("=");
         if (parts[1].trim().matches("\".*\"") ) {
-            try{
-                String val =parts[1];
-                Matcher m = Pattern.compile("(.*?)"/*"([\\\"'])(?:(?=(\\\\?))\\2.)*?\\1"*/).matcher(val);
-                if (m.find()) {
-                    val = m.group(1).replace("\"", "");
-                    inte.put(parts[0].trim(), val);
-                }else {
-                    System.err.println("String syntax error");
-                }
-            }catch (Exception e) {
-                e.printStackTrace();
-            }
+
         }
         else{
             try {
                 Object val = Expression.eval(inte.getVars(), parts[1]);
-                inte.put(parts[0].trim(), Double.parseDouble(val.toString()));
+                inte.getVars().put(parts[0].trim(), Double.parseDouble(val.toString()));
             } catch (ScriptException e) {
                 e.printStackTrace();
             }
